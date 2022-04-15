3e02edfcc8c9a551f6c8b4526bfc495c7818590f
- <=>
2022-04-15 22:19:20 +0700

    first commit

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -1,8 +1,19 @@
 package com.kenIT;
 
 public class Main {
+    private static AccountExample accountExample;
+    public static final String[] validAccount = new String[] { "123abc_", "_abc123", "______", "123456","abcdefgh" };
+    public static final String[] invalidAccount = new String[] { ".@", "12345", "1234_", "abcde" };
 
     public static void main(String[] args) {
-	// write your code here
+        accountExample = new AccountExample();
+        for (String account : validAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Account is " + account +" is valid: "+ isValid);
+        }
+        for (String account : invalidAccount) {
+            boolean isValid = accountExample.validate(account);
+            System.out.println("Account is " + account +" is valid: "+ isValid);
+        }
     }
 }
